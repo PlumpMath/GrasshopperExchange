8c3df1b5a1c4c305bea4963d6fa64a54a0014fdc
provolot <provolot@gmail.com>
2013-11-30 19:35:54 -0500

    uploading example

--- Hairworm/HairwormComponent.cs
+++ Hairworm/HairwormComponent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Special;
 using Rhino.Geometry;
 
 namespace Hairworm
@@ -16,9 +18,9 @@ namespace Hairworm
         /// new tabs/panels will automatically be created.
         /// </summary>
         public HairwormComponent()
-            : base("Hairworm", "Nickname",
+            : base("Hairworm", "Hairworm",
                 "Description",
-                "Category", "Subcategory")
+                "Extra", "Hairworm")
         {
         }
 
@@ -27,6 +29,9 @@ namespace Hairworm
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddTextParameter("String", "ClusterPath", "Path To Cluster", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Activate", "Activate", "Activate to emulate clsuter", GH_ParamAccess.item, false);
+            pManager.AddGeometryParameter("Input Geometry", "InputGeo", "InputGeometry", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -34,6 +39,8 @@ namespace Hairworm
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddGeometryParameter("Output Geometry", "OutputGeo", "OutputGeometry", GH_ParamAccess.tree);
+            pManager.AddGenericParameter("Generic Output", "GenericOutput", "GenericOutput", GH_ParamAccess.tree);
         }
 
         /// <summary>
@@ -43,6 +50,58 @@ namespace Hairworm
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+
+            // Declare a variable for the input String
+            string filename = null;
+            bool activate = false;
+            Rhino.Geometry.Point3d point = Rhino.Geometry.Point3d.Unset;
+            // 1. Declare placeholder variables and assign initial invalid data.
+            //    This way, if the input parameters fail to supply valid data, we know when to abort.
+
+            // 2. Retrieve input data, exit if non-existent
+            if (!DA.GetData(0, ref filename)) { return; }
+            if (!DA.GetData(1, ref activate)) { return; }
+            if (!DA.GetData(2, ref point)) { return; }
+
+            // If the retrieved data is Nothing, we need to abort.
+            if (filename == null) { return; }
+            if (!File.Exists(filename)) { return; }
+
+            if (activate)
+            {
+                GH_Cluster cluster = new GH_Cluster();
+                cluster.CreateFromFilePath(filename);
+
+                GH_Document doc = OnPingDocument();
+                doc.AddObject(cluster, false);
+
+                Grasshopper.Kernel.Parameters.Param_Point paramIn = new Grasshopper.Kernel.Parameters.Param_Point();
+                Grasshopper.Kernel.Parameters.Param_Geometry paramOut = new Grasshopper.Kernel.Parameters.Param_Geometry();
+
+                paramIn.SetPersistentData(point);
+
+                cluster.Params.RegisterInputParam(paramIn);
+                cluster.Params.RegisterOutputParam(paramOut);
+
+                cluster.CollectData();
+
+                cluster.ComputeData();
+
+
+                //Grasshopper.DataTree<object> test = new DataTree<object>();
+                //test.Add(paramIn, 0);
+
+
+
+                DA.SetData(1, paramOut);
+
+
+
+
+
+
+
+            }
         }
 
         /// <summary>
